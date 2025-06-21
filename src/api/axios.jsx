e10694e22c845a6ccfd91aa069f40b43d926ce1e
Anjali Tiwari <riya.tiwari9821@gmail.com>
2025-06-21 13:51:07 +0530

    Update axios.jsx

--- src/api/axios.jsx
+++ src/api/axios.jsx
@@ -3,7 +3,7 @@ import axios from 'axios';
 // Create an Axios instance
 const apiClient = axios.create({
   baseURL: '${import.meta.env.VITE_SERVER_URL}/api', // Replace with your API's base URL
-  timeout: 10000, // Timeout in milliseconds
+  timeout: 100000, // Timeout in milliseconds
 });
 
 export default apiClient;
